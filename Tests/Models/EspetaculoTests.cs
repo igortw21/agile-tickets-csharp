2562f6010f3ebe092c8f0ccc276a1131e0756c14
CJM <>
2014-05-23 21:56:19 -0300

    Criacao de mais testes de unidade para a funcionalidade de CriarSessoes

--- Tests/Models/EspetaculoTests.cs
+++ Tests/Models/EspetaculoTests.cs
@@ -93,7 +93,7 @@ namespace Tests.Models
 
             IList<Sessao> sessoes = ironMaiden.CriaSessoes(dateInicioFim, dateInicioFim, Periodicidade.SEMANAL);
 
-            Assert.AreEqual(0, sessoes.Count());
+            Assert.AreEqual(1, sessoes.Count());
         }
 
         [Test]
@@ -109,7 +109,7 @@ namespace Tests.Models
         }
 
         [Test]
-        public void DeveCriar2SessoesQuandoDataFimEh2DiasDepoisDeDataInicioEhAPeriodicidadeEhDiaria()
+        public void DeveCriar3SessoesQuandoDataFimEh2DiasDepoisDeDataInicioEhAPeriodicidadeEhDiaria()
         {
             Espetaculo ironMaiden = new Espetaculo();
 
@@ -118,7 +118,7 @@ namespace Tests.Models
 
             IList<Sessao> sessoes = ironMaiden.CriaSessoes(dateInicio, dataFim, Periodicidade.DIARIA);
 
-            Assert.AreEqual(2, sessoes.Count());
+            Assert.AreEqual(3, sessoes.Count());
         }
 
         [Test]
@@ -131,11 +131,11 @@ namespace Tests.Models
 
             IList<Sessao> sessoes = ironMaiden.CriaSessoes(dateInicio, dataFim, Periodicidade.SEMANAL);
 
-            Assert.AreEqual(1, sessoes.Count());
+            Assert.AreEqual(2, sessoes.Count());
         }
 
         [Test]
-        public void NaoDeveCriarSessoesQuandoDataFimEh6DiasDepoisDeDataInicioEhAPeriodicidadeEhSemanal()
+        public void DeveCriar1SessaoQuandoDataFimEh6DiasDepoisDeDataInicioEhAPeriodicidadeEhSemanal()
         {
             Espetaculo ironMaiden = new Espetaculo();
 
@@ -144,10 +144,46 @@ namespace Tests.Models
 
             IList<Sessao> sessoes = ironMaiden.CriaSessoes(dateInicio, dataFim, Periodicidade.SEMANAL);
 
-            Assert.AreEqual(0, sessoes.Count());
+            Assert.AreEqual(1, sessoes.Count());
         }
 
-        #endregion
+        [Test]
+        public void DevePreencherSessoesComDadosDoEspetaculo()
+        {
+            Espetaculo ironMaiden = SetUpEstabelecimento();
+
+            DateTime dataInicio = new DateTime();
+            DateTime dataFim = dataInicio;
+
+            IList<Sessao> sessoes = ironMaiden.CriaSessoes(dataInicio, dataFim, Periodicidade.DIARIA);
+
+            Assert.AreEqual(1, sessoes.Count());
+
+            VerificarEstabelecimento(sessoes[0].Espetaculo);
+        }
+
+        private Espetaculo SetUpEstabelecimento()
+        {
+            Espetaculo ironMaiden = new Espetaculo();
+            ironMaiden.Id = 1;
+            ironMaiden.Descricao = "Metal!!!";
+            ironMaiden.Estabelecimento = new Estabelecimento();
+            ironMaiden.Estabelecimento.Id = 1;
+            ironMaiden.Estabelecimento.Nome = "Citibank Hall";
+            ironMaiden.Estabelecimento.Endereco = "Rua ABCD";
+            return ironMaiden;
+        }
+
+        private void VerificarEstabelecimento(Espetaculo espetaculo)
+        {
+            Assert.AreEqual(1, espetaculo.Id);
+            Assert.AreEqual("Metal!!!", espetaculo.Descricao);
+            Assert.AreEqual(1, espetaculo.Estabelecimento.Id);
+            Assert.AreEqual("Citibank Hall", espetaculo.Estabelecimento.Nome);
+            Assert.AreEqual("Rua ABCD", espetaculo.Estabelecimento.Endereco);
+        }
+
+        #endregion       
 
         private Sessao SessaoComIngressosSobrando(int quantidade)
         {
